2d660697225528b8d9322008bf590239931b5be4
Uliana Gritchina <ugritchina@icloud.com>
2023-08-10 18:57:52 +0300

    Initial Commit

---
+++ NFCSwiftUI/NFCSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NFCSwiftUIApp.swift
+//  NFCSwiftUI
+//
+//  Created by Ульяна Гритчина on 10.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct NFCSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
